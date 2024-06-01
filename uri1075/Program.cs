5534b44fe054a29099a3fc37d1d9a21dd0a8c051
wesslingpedro <wesslingpedro@gmail.com>
2024-06-01 11:54:49 -0300

    first commit

---
+++ uri1075/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_ec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 10000)
+            {
+                for (int x = 1; x <= 10000; x++)
+                {
+                    if (x % n == 2)
+                    {
+                        Console.WriteLine(x);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
